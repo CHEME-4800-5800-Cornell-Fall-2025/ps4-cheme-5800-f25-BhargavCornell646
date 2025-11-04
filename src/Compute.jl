8bb9d0eb994501eb3048e96a24c9fe056837e91c
BhargavCornell646 <bh646@cornell.edu>
2025-11-04 16:17:16 -0500

    .

--- src/Compute.jl
+++ src/Compute.jl
@@ -72,9 +72,35 @@ function solve(model::MySimulatedAnnealingMinimumVariancePortfolioAllocationProb
     while has_converged == false
     
         accepted_counter = 0; 
+         for _ in 1:KL
+       
+            # generate a new candidate solution -
+            candidate_w = current_w + β * randn(length(w));
+            
+            #Removing the barrier term
+            #candidate_w = max.(0.0, candidate_w);
+
+            # compute the objective function at the candidate solution -
+            candidate_f = _objective_function(candidate_w, ḡ, Σ̂, R, μ, ρ);
+
+            # accept or reject the candidate solution -
+            if candidate_f < current_f || rand() < exp((current_f - candidate_f) / T)
+                current_w = candidate_w;
+                current_f = candidate_f;
+
+                # we've accepeted this move, update the counter -
+                accepted_counter += 1;
+            end
         
-        # TODO: Implement simulated annealing logic here -
-        throw(ErrorException("Oooops! Simulated annealing logic not yet implemented!!"));
+            # Compute the diff between current and best solution found so far
+            if (current_f < f_best)
+                w_best = current_w;
+                f_best = current_f;
+            end
+        end
+
+        #COMPLETED: Implement simulated annealing logic here -
+        #throw(ErrorException("Oooops! Simulated annealing logic not yet implemented!!"));
 
         # update KL -
         fraction_accepted = accepted_counter/KL; # what is the fraction of accepted moves
